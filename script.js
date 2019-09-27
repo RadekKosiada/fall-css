f75e6ea6b0ac2bb9f521c26436335e3a6a797ecc
r.kosiada <r.kosiada@hirschtec.eu>
2019-09-27 16:44:56 +0200

    Format js fle and its comments

--- script.js
+++ script.js
@@ -16,67 +16,86 @@ let colorObj = {
 }
 // picking a random color from the object
 function randomColor() {
-let colKey = Math.floor((Math.random() * Object.keys(colorObj).length));
-return Object.entries(colorObj)[colKey][1]; // using ES6 Object.entries to get a random property from our colorObj. We need only value, the rgb color, thus [1].
+  let colKey = Math.floor((Math.random() * Object.keys(colorObj).length));
+  // using ES6 Object.entries to get a random property from our colorObj.
+  // We need only value, the rgb color, thus [1].
+  return Object.entries(colorObj)[colKey][1];  
+  
+  
 };
 
-function update () {
-  for (let i = pieces.length -1; i >=0; i--) {
-    let p = pieces[i]; // the for loop iterates through the pieces array, starting from the one before last; runs under the condition that i equals or larger than 0; i decreases by 1, each time it iterates.  
+function update() {
+  for (let i = pieces.length - 1; i >= 0; i--) {
+    // the for loop iterates through the pieces array, starting from the one before last; 
+    // runs under the condition that i equals or larger than 0; i decreases by 1, each time it iterates.  
 
+    let p = pieces[i]; 
     if (p.y > canvas.height) {
-      pieces.splice(i, 1); //if the piece reaches the lower border of the canvas, it's getting removed (*1* element) from the array on the place *i*. 
+      //if the piece reaches the lower border of the canvas, 
+      // it's getting removed (*1* element) from the array on the place *i*. 
+      pieces.splice(i, 1); 
       continue; //after it removes the element, the iteration and continues from there on.
     }
 
     while (pieces.length < numberOfPieces) {
       pieces.push(new piece(Math.random() * canvas.width, -20)); // adding new pieces to the array: pieces; with x & y
     }
-    
+
     p.y += p.gravity; //position of a piece on the y axe is being changed by as much as defined in gravity.
-    p.rotation += p.rotationSpeed; 
+    p.rotation += p.rotationSpeed;
   }
   setTimeout(update, 1); //the function will be called after 1 millisecond
 }
 
-function draw () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //this will prevent that the pieces will stay on the canvas, snake-like. 
-  
-    pieces.forEach(function (p) { //iterates through all elements in the array pieces
-      ctx.save(); //we need to save the canvases and restore it below, to make just the pieces rotate.
-      ctx.fillStyle = p.color;
-      ctx.translate(p.x +p.size/2, p.y +p.size/2); // it will translate the canvas to the center of the specific component;its position + half of its size. 
-      ctx.rotate(p.rotation); //we set rotation in the piece function; 
-  
-      ctx.fillRect(-p.size / 2, -p.size/2, p.size, p.size); // this draws a new element: it is placed at 0, 0 of the translated canvas, thus *-p.size* https://www.screencast.com/t/DSKpt7NxH; https://www.w3schools.com/graphics/game_rotation.asp
-      
-      ctx.restore(); // this will stop from transforming other elements. 
-    })
-    requestAnimationFrame(draw); //to update the animation on screen
-  }
-  
-  //this function will define how the confetti will be like
-  //it will store all important features
-  function piece (x, y) {
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //this will prevent that the pieces will stay on the canvas, snake-like. 
+
+  pieces.forEach(function (p) { //iterates through all elements in the array pieces
+    ctx.save(); //we need to save the canvases and restore it below, to make just the pieces rotate.
+    ctx.fillStyle = p.color;
+    // it will translate the canvas to the center of the specific component;
+    // its position + half of its size. 
+    ctx.translate(p.x + p.size / 2, p.y + p.size / 2); 
+    ctx.rotate(p.rotation); //we set rotation in the piece function; 
+
+    //// square pieces
+    // this draws a new element: it is placed at 0, 0 of the translated canvas, 
+    // thus *-p.size* https://www.screencast.com/t/DSKpt7NxH; https://www.w3schools.com/graphics/game_rotation.asp
+    ctx.stroke();
+    ctx.fillRect(-p.size / 2, -p.size / 2, p.size, p.size); 
+
+    ////circle pieces
+    // ctx.beginPath();
+    // ctx.arc(100, 75, p.size, 0, 2 * Math.PI);
+    // ctx.fill();
+
+    ctx.restore(); // this will stop from transforming other elements. 
+  })
+  requestAnimationFrame(draw); //to update the animation on screen
+}
+
+//this function will define how the confetti will be like
+//it will store all important features
+function piece(x, y) {
   this.x = x; //this refers to global object this === window; the variable remains global
   this.y = y;
-  this.size = (Math.random() * 0.5 + 0.75)* 15;
-  this.gravity = (Math.random() * 0.5+0.75)* 1  //the gravity can be in a similar range to size of the piece. 15 would be to fast. addtionally check *gravitySpeed*.
+  this.size = (Math.random() * 0.5 + 0.75) * 15;
+  //the gravity can be in a similar range to size of the piece. 15 would be to fast. 
+  // addtionally check *gravitySpeed*.
+  this.gravity = (Math.random() * 0.5 + 0.75) * 1  
   this.rotation = (Math.PI * 2) * Math.random(); //how much we rotate the element;
   this.rotationSpeed = (Math.PI * 2) * Math.random() * 0.010; //how fast we rotate it;
   this.color = randomColor();
-  
-  }
-  
-  while (pieces.length < numberOfPieces) {
-    //calling piece(x, y) function
-    pieces.push(new piece(Math.random() * canvas.width, Math.random() * canvas.height)); // adding new pieces to the array: pieces; with x & y
-  }
-  // both functions will call themselves
-  update(); 
-  draw();
+
+}
+
+while (pieces.length < numberOfPieces) {
+  //calling piece(x, y) function
+  pieces.push(new piece(Math.random() * canvas.width, Math.random() * canvas.height)); // adding new pieces to the array: pieces; with x & y
+}
+// both functions will call themselves
+update();
+draw();
 
 
-  ctx.beginPath();
-ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-ctx.stroke();
+
