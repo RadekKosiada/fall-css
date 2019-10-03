d74d1932e15a361b183d3376238e996c2ffd9dd8
RadekKosiada <kosiadar@gmail.com>
2019-10-03 20:31:21 +0200

    Define flexible number of squares & left position

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-var numberOfSquares = 70;
-var colors = [
-  "yellow", "blue", "red", "orange", "green"
-];
+var distanceBetweenSquares = 20;
+var squareSize = 10;
+// number of squares depends on the width of window (more mobile-friendly/responsive approach)
+var numberOfSquares = Math.round(window.innerWidth/distanceBetweenSquares);
+console.log(numberOfSquares);
+var colors = ["yellow", "blue", "red", "orange", "green"];
 var numberOfColors = colors.length;
 
 for (var i = 0; i < numberOfSquares; i++) {
   //creating elements
   var square = document.createElement("div");
   square.setAttribute("class", "square");
+
   //style
-  square.style.width = "10px";
-  square.style.height = "10px";
+  square.style.width = squareSize + "px";
+  square.style.height = square.style.width;
   // adding random background color
   square.style.backgroundColor = colors[randomNumber(numberOfColors)];
   square.style.position = "fixed";
   square.style.zIndex = "-1";
-  square.style.top = 100 + 10*i + "px";
-  square.style.left = 100 + 10*i + "px";
-
+  square.style.top = 0 + "px";
+  // setting left position for each square; each will 
+  square.style.left = (window.innerWidth / numberOfSquares) * i + "px";
   // appending to body
   document.body.appendChild(square);
 }
 
 
-console.log(document.getElementsByClassName("square"));
-
-// random position 
+// random position
 // movement
 // loop? or setInterval? or CSS animation!!!
 // 3 animacje rotating
 // 7 animacji ruchu
 // i dodac je randomly do squares;
 
+
+// supporting functions
+
+// returns a random integer from 0 to num-1;
 function randomNumber(num) {
   return Math.floor(Math.random() * Math.floor(num));
 }
