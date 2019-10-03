204669b55e708c86a44a33729f783ff0ff4063fe
RadekKosiada <kosiadar@gmail.com>
2019-10-03 20:55:26 +0200

    Wrap for loop in a function

--- script.js
+++ script.js
@@ -2,30 +2,36 @@ var distanceBetweenSquares = 20;
 var squareSize = 10;
 // number of squares depends on the width of window (more mobile-friendly/responsive approach);
 // their density will be always the same, regardless the size of the screen;
+
 var numberOfSquares = Math.round(window.innerWidth/distanceBetweenSquares);
 console.log(numberOfSquares);
+
 var colors = ["yellow", "blue", "red", "orange", "green"];
 var numberOfColors = colors.length;
 
-for (var i = 0; i < numberOfSquares; i++) {
-  //creating elements
-  var square = document.createElement("div");
-  square.setAttribute("class", "square");
-
-  //style
-  square.style.width = squareSize + "px";
-  square.style.height = square.style.width;
-  // adding random background color
-  square.style.backgroundColor = colors[randomNumber(numberOfColors)];
-  square.style.position = "fixed";
-  square.style.zIndex = "-1";
-  square.style.top = 0 + "px";
-  // setting left position for each square with even distance between them
-  square.style.left = (window.innerWidth / numberOfSquares) * i + "px";
-  // appending to body
-  document.body.appendChild(square);
+function createSquares() {
+  for (var i = 0; i < numberOfSquares; i++) {
+    //creating elements
+    var square = document.createElement("div");
+    square.setAttribute("class", "square");
+  
+    //style
+    square.style.width = squareSize + "px";
+    square.style.height = square.style.width;
+    // adding random background color
+    square.style.backgroundColor = colors[randomNumber(numberOfColors)];
+    square.style.position = "fixed";
+    square.style.zIndex = "-1";
+    square.style.top = 0 + "px";
+    // setting left position for each square with even distance between them
+    square.style.left = (window.innerWidth / numberOfSquares) * i + "px";
+    // appending to body
+    document.body.appendChild(square);
+  }
 }
 
+createSquares();
+// setInterval(createSquares, 1000);
 
 // random position
 // movement
@@ -33,6 +39,14 @@ for (var i = 0; i < numberOfSquares; i++) {
 // 3 animacje rotating
 // 7 animacji ruchu
 // i dodac je randomly do squares;
+
+// add window resize event
+// var num = window.addEventListener("resize", function () {
+//   console.log(window.innerWidth)
+//   return window.innerWidth;
+// });
+
+// console.log(num)
 
 
 // supporting functions
