bbcf40f6e587a91779a17ac996559a7e2da4ac65
RadekKosiada <kosiadar@gmail.com>
2019-10-03 19:44:32 +0200

    Add notes & colors array

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-var squaresArray = [];
 var numberOfSquares = 70;
+var colors = [
+  "yellow", "blue", "red", "orange", "green"
+];
+var numberOfColors = colors.length;
 
 for (var i = 0; i < numberOfSquares; i++) {
   //creating elements
   var square = document.createElement("div");
+  square.setAttribute("class", "square");
   
   //style
-  square.style.width = "8px";
-  square.style.height = "8px";
+  square.style.width = "10px";
+  square.style.height = "10px";
   square.style.backgroundColor = "purple";
   square.style.position = "fixed";
+  square.style.zIndex = "-1";
   square.style.top = 100 + 10*i + "px";
   square.style.left = 100 + 10*i + "px";
 
-  // appendding to body
+  // appending to body
   document.body.appendChild(square);
 }
+
+
+console.log(document.getElementsByClassName("square"));
+
+// random color
+// random position 
+// movement
+// loop? or setInterval? or CSS animation!!!
+// 3 animacje rotating
+// 7 animacji ruchu
+// i dodac je randomly do squares;
+
