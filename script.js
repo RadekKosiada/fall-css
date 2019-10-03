e5d25f455bc6bc46fe385ae96915e30e4f3c558f
RadekKosiada <kosiadar@gmail.com>
2019-10-03 22:39:52 +0200

    Add rotation

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ var distanceBetweenSquares = 20;
 var squareSize = 10;
 // number of squares depends on the width of window (more mobile-friendly/responsive approach);
 // their density will be always the same, regardless the size of the screen;
-
-var numberOfSquares = Math.round(window.innerWidth/distanceBetweenSquares);
+var numberOfSquares = 4*Math.round(window.innerWidth/distanceBetweenSquares);
 console.log(numberOfSquares);
 
 var colors = ["yellow", "blue", "red", "orange", "green"];
 var numberOfColors = colors.length;
+
+var animationDirection = ["normal", "reverse"];
+var animationDirectionNumber = animationDirection.length;
+ 
+var maxRotationSpeed = 1.5;
+var minRotationSpeed = 0.25;
 
 function createSquares() {
   for (var i = 0; i < numberOfSquares; i++) {
@@ -19,19 +24,26 @@ function createSquares() {
     square.style.width = squareSize + "px";
     square.style.height = square.style.width;
     // adding random background color
-    square.style.backgroundColor = colors[randomNumber(numberOfColors)];
+    square.style.backgroundColor = colors[randomInteger(numberOfColors)];
     square.style.position = "fixed";
     square.style.zIndex = "-1";
-    square.style.top = 0 + "px";
+    square.style.top = "0px";
     // setting left position for each square with even distance between them
     square.style.left = (window.innerWidth / numberOfSquares) * i + "px";
+    // setting animation
+    square.style.animationName = "rotation";
+    square.style.animationTimingFunction = "linear";
+    square.style.animationIterationCount ="infinite";
+    // direction will randomly picked from predefined array;
+    square.style.animationDirection = animationDirection[randomInteger(animationDirectionNumber)];
+    square.style.animationDuration = randomNumber(minRotationSpeed, maxRotationSpeed) + "s"; 
     // appending to body
     document.body.appendChild(square);
   }
 }
 
 createSquares();
-// setInterval(createSquares, 1000);
+//setInterval(createSquares, 2000);
 
 // random position
 // movement
@@ -52,6 +64,11 @@ createSquares();
 // supporting functions
 
 // returns a random integer from 0 to num-1;
-function randomNumber(num) {
+function randomInteger(num) {
   return Math.floor(Math.random() * Math.floor(num));
-}
+};
+
+// returns a random number larger or equal than min and smaller (not equal) than max
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+};
